fde44dfb381745f1c325311a7715557cd98fbcd0
DESKTOP-GBGOJT7\Atian <anton.demagin@yandex.ru>
2023-11-18 15:57:17 +0300

    Added colidres to all object
    Added 2D coliders to all gameobject.
    Added blocking movement logic for player.

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    private Rigidbody2D _rigidbody2d;
+    private float _horizontal;
+    private float _vertical;
+
     // Start is called before the first frame update
     void Start()
     {
+        _rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        _horizontal = Input.GetAxis("Horizontal");
+        _vertical = Input.GetAxis("Vertical");
+    }
 
-        Vector2 position = transform.position;
-        position.x = position.x + 3f * horizontal * Time.deltaTime;
-        position.y = position.y + 3f * vertical * Time.deltaTime;
-        transform.position = position;
+    void FixedUpdate()
+    {
+        Vector2 position = _rigidbody2d.position;
+        position.x = position.x + 3f * _horizontal * Time.deltaTime;
+        position.y = position.y + 3f * _vertical * Time.deltaTime;
+        _rigidbody2d.MovePosition(position);
     }
 }
