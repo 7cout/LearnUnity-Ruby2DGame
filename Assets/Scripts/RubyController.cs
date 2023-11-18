e1c88cd62c64247e72febf498b9a2584c2c03a18
DESKTOP-GBGOJT7\Atian <anton.demagin@yandex.ru>
2023-11-18 16:16:09 +0300

    Add health attribute
    Add health attribute

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RubyController : MonoBehaviour
-{
+{   
     private Rigidbody2D _rigidbody2d;
     private float _horizontal;
     private float _vertical;
+    private int _currentHealth;
+
+    public int maxHealth;
 
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody2d = GetComponent<Rigidbody2D>();
+
+        _currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -27,5 +32,11 @@ public class RubyController : MonoBehaviour
         position.x = position.x + 3f * _horizontal * Time.deltaTime;
         position.y = position.y + 3f * _vertical * Time.deltaTime;
         _rigidbody2d.MovePosition(position);
+    }
+
+    void ChangeHealth( int amount )
+    {
+        _currentHealth = Mathf.Clamp(_currentHealth + amount, 0, maxHealth);
+        Debug.Log(_currentHealth + "/" + maxHealth);
     }
 }
